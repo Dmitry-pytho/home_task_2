63b88353e252ad4f662962670c25cecf207c3ff9
Dmitry-pytho <dmitry.solomiannyi@gmail.com>
2021-03-21 21:35:35 +0200

    added end_string

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def parse_cookie(query: str) -> dict:
         cookies[key] = morsel.value
     return cookies
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
